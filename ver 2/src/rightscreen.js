537e14891c1a6569b7a97709274524f985580fb2
byhhh2 <rlaalsdl111@gmail.com>
2021-03-11 23:10:40 +0900

    correct error

--- ver 2/src/rightscreen.js
+++ ver 2/src/rightscreen.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./rightscreen.css";
 import HomeRight from "./home/HomeRight.js";
+import Visitor from "./visitor/VisitorsBook";
+
 class RightScreen extends Component {
   render() {
     return (
       <div id="ifright">
-        <HomeRight />
+        {/*<HomeRight />*/}
+        <Visitor />
       </div>
     );
   }
