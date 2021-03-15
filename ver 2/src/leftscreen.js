9f1e7bbf4e0c0c7195c350b47d0f061ddbea995e
byhhh2 <rlaalsdl111@gmail.com>
2021-03-16 03:26:09 +0900

    add feature, fix error

--- ver 2/src/leftscreen.js
+++ ver 2/src/leftscreen.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import "./leftscreen.css";
 import HomeLeft from "./home/HomeLeft.js";
+import PhotoL from "./photo/PhotoL";
+
 class LeftScreen extends Component {
   render() {
+    let screenState = this.props.rightScreenState;
     return (
       <div id="ifleft">
-        <HomeLeft />
+        {screenState === 1 ? (
+          <HomeLeft />
+        ) : screenState === 2 ? (
+          <></>
+        ) : screenState === 3 ? (
+          <></>
+        ) : screenState === 4 ? (
+          <PhotoL screenState={screenState} />
+        ) : screenState === 5 ? (
+          <HomeLeft />
+        ) : (
+          <></>
+        )}
       </div>
     );
   }
